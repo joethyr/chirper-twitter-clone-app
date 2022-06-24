5f5ffd2ed05be340a2be314cc4771ddd25939f11
joethyr <joethayr@gmail.com>
2022-06-23 20:43:39 -0600

    update sign up form with Bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     registrations: 'registrations'
   }
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   resources :chirps
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
